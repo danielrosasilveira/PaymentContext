b8b39af218c63df3dab57c2121141f931d0e0cf9
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2020-04-11 18:48:06 -0300

    Initial Commit

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,38 @@
+﻿using PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        protected Payment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string owner, Document document, Address address, Email email)
+        {
+            Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,10).ToUpper();
+            PaidDate = paidDate;
+            ExpireDate = expireDate;
+            Total = total;
+            TotalPaid = totalPaid;
+            Owner = owner;
+            Document = document;
+            Address = address;
+            Email = email;
+        }
+
+        //Livro Clean Code - Robert C. Martin (Uncle BOB)
+        //Livro Clean Coder - Robert C. Martin
+        //Livro Clean Architecture - Robert C. Martin
+
+        public string Number { get; private set; }
+        public DateTime PaidDate { get; private set; }
+        public DateTime ExpireDate { get; private set; }
+        public Decimal Total { get; private set; }
+        public Decimal TotalPaid { get; private set; }
+        public string Owner { get; private set; }
+        public Document Document { get; private set; }
+        public Address Address { get; private set; }
+        public Email Email { get; private set; }
+    }
+    
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            //var subscription = new Subscription(null);
+            //var student = new Student("Daniel","Rosa","123456","danielrosasilveira@gmail.com");
+            //student.AddSubscription(subscription);
+        }
+    }
+}
